16ba0a681b2993de962a5006b54413b1cfcb0a1c
KimRiHyun <lookau1004@gmail.com>
2022-05-01 14:40:09 +0900

    제스처 트레이닝 기본 형태 추가

---
+++ ksu_hm/gesture_train.py
@@ -0,0 +1,79 @@
+from asyncio.windows_events import NULL
+import cv2
+import mediapipe as mp
+import numpy as np
+
+class TrainCamara(): 
+    def __init__(self):    
+        self.max_num_hands = 1        
+        self.gesture = {
+            0:'fist', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+            6:'six', 7:'rock', 8:'spiderman', 9:'yeah', 10:'ok', 11:'fy'
+        }        
+        self.mp_hands = mp.solutions.hands
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.hands = self.mp_hands.Hands(
+            max_num_hands=self.max_num_hands,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+        
+        self.file = np.genfromtxt('Data/gesture_train.csv', delimiter=',')
+    
+    def click(self,event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.file = np.vstack((self.file, self.data))
+            print(self.file.shape)
+    
+    def CamaraStart(self):
+        print(self.file.shape)
+
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+        cv2.namedWindow('Dataset')
+        cv2.setMouseCallback('Dataset', self.click)
+
+        while cap.isOpened():
+            ret, img = cap.read()
+            if not ret:
+                continue
+
+            img = cv2.flip(img, 1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            result = self.hands.process(img)
+
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+            if result.multi_hand_landmarks is not None:
+                for res in result.multi_hand_landmarks:
+                    joint = np.zeros((21, 3))
+                    for j, lm in enumerate(res.landmark):
+                        joint[j] = [lm.x, lm.y, lm.z]
+
+                    # Compute angles between joints
+                    v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
+                    v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+                    v = v2 - v1 # [20,3]
+                    # Normalize v
+                    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                    # Get angle using arcos of dot product
+                    angle = np.arccos(np.einsum('nt,nt->n',
+                        v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                        v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+
+                    angle = np.degrees(angle) # Convert radian to degree
+
+                    self.data = np.array([angle], dtype=np.float32)
+                    self.data = np.append(self.data, 11)
+
+                    self.mp_drawing.draw_landmarks(img, res, self.mp_hands.HAND_CONNECTIONS)
+
+            cv2.imshow('Dataset', img)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        np.savetxt('Data/gesture_train_fy.csv', self.file, fmt='%f',delimiter=',')
+
+newCamara = TrainCamara()
+newCamara.CamaraStart()
